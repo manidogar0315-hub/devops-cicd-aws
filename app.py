5557d2b371581fee4acf80e75adb5277ebf67f9b
Abdul Rehman Dogar <manidogar0315@gmail.com>
2026-01-27 17:33:03 +0500

    chore: update app message to test CI/CD

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps CI/CD on AWS is LIVE 🚀"
+    return "DevOps CI/CD on AWS is LIVE 🚀 and after day 3, it is pushed to docker automatically"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
