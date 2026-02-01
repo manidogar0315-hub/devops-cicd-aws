6fa8a644808a2b185509673253fed0f4519686d1
Abdul Rehman Dogar <manidogar0315@gmail.com>
2026-02-01 14:57:10 +0500

    feat: add professional UI and fix Dockerfile to include templates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps CI/CD on AWS is LIVE 🚀 and after day 3, it is pushed to docker automatically"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
--- dockerfile
+++ dockerfile
@@ -2,10 +2,9 @@ FROM python:3.10-slim
 
 WORKDIR /app
 
-COPY requirements.txt .
-RUN pip install -r requirements.txt
+COPY . .
 
-COPY app.py .
+RUN pip install -r requirements.txt
 
 EXPOSE 5000
 
